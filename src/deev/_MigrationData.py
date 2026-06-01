cac9637d23047fceec2b88b96248154933cbe212
wilson0x4d <mrshaunwilson@msn.com>
2026-06-01 13:32:47 -0500

    fix(sqlite): remove incompatible sqltype override

--- src/deev/_MigrationData.py
+++ src/deev/_MigrationData.py
@@ -7,14 +7,9 @@ from .entities import entity, field
 @entity(table_name='_migrationdata')
 class _MigrationData:  # type: ignore[misc]
     """Internal entity representation of ``_migrationdata`` tables used by ``deev``."""
-    id: int = field(
-        autoincrement=True,
-        primary_key=True
-    )
-    migration: str = field(
-        max=260,
-        sqltype='VARCVHAR(260)'
-    )
+
+    id: int = field(autoincrement=True, primary_key=True)
+    migration: str = field(max=260)
 
 
 __all__ = ['_MigrationData']
