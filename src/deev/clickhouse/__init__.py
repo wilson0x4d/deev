707d8cf67b680ac8c148e61be11100646ed7ccd2
wilson0x4d <mrshaunwilson@msn.com>
2026-08-12 00:48:20 -0500

    fix: import failure

--- src/deev/clickhouse/__init__.py
+++ src/deev/clickhouse/__init__.py
@@ -1,11 +1,15 @@
 # SPDX-FileCopyrightText: © 2026 Shaun Wilson
 # SPDX-License-Identifier: MIT
 
-from .clickhouse_proxy_connection import ClickHouseProxyConnection
-from .clickhouse_proxy_cursor import ClickHouseProxyCursor
-from .clickhouse_table_adapter import ClickHouseTableAdapter
-from .clickhouse_transaction_context import ClickHouseTransactionContext
-from .clickhouse_type_mapper import ClickHouseTypeMapper
+try:
+    from .clickhouse_proxy_connection import ClickHouseProxyConnection
+    from .clickhouse_proxy_cursor import ClickHouseProxyCursor
+    from .clickhouse_table_adapter import ClickHouseTableAdapter
+    from .clickhouse_transaction_context import ClickHouseTransactionContext
+    from .clickhouse_type_mapper import ClickHouseTypeMapper
+except Exception:
+    # NOTE: if required packages are not installed we expect this module to import without errors
+    pass
 
 
 __all__ = [
